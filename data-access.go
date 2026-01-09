345e491d0390fa22c73f60bb06d18a3bc2282f81
beatricepang <s3956978@student.rmit.edu.au>
2026-01-09 15:23:36 +1100

    connection established and data returned to the terminal !

--- data-access.go
+++ data-access.go
@@ -34,24 +34,43 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// ping error to terminal
 	pingErr := db.Ping()
 	if pingErr != nil {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected!")
+
+	albums, err := albumByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Albums found: %v\n", albums)
+
 }
 
 func albumByArtist(name string) ([]Album, error) {
+	// create an array of album object for the album data
 	var albums []Album
-
+	// collect the data from the artist names in the db
+	// by writting it this way it prevents injection risks.
 	rows, err := db.Query("SELECT * FROM album WHERE artist =?", name)
 
+	// error handling for if the
 	if err != nil {
 		return nil, fmt.Errorf("albumsByArtist %q: %v, name, err")
 	}
+	// any resoruce it holds is released when the function exists
 	defer rows.Close()
 
+	// loop the returned rows through row.scan for the album struct fields
+	// scans for a list of pointers to the go value
+	// collumn values are written
+	// inside the loop check for an error scanning the column values within the struct fields
+	// append the new alb to the album slice
 	for rows.Next() {
+
 		var alb Album
 		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
 			return nil, fmt.Errorf("albumsByArtist %q; %v", name, err)
