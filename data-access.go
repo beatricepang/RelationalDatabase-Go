abf941ada1c9b959d21e3d12e6ac26a749bf3e96
beatricepang <s3956978@student.rmit.edu.au>
2026-01-09 15:53:12 +1100

    adding a single row of query data

--- data-access.go
+++ data-access.go
@@ -48,8 +48,15 @@ func main() {
 
 	fmt.Printf("Albums found: %v\n", albums)
 
+	alb, err := albumByID(2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
+
 }
 
+// QUERY FOR A SINGLE DATA FIELD
 func albumByArtist(name string) ([]Album, error) {
 	// create an array of album object for the album data
 	var albums []Album
@@ -83,4 +90,18 @@ func albumByArtist(name string) ([]Album, error) {
 
 	}
 	return albums, nil
+}
+
+// QUERY FOR A SINGLE ROW
+
+func albumByID(id int64) (Album, error) {
+	var alb Album
+	row := db.QueryRow("SELECT * FROM album WHERE id=?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumsById %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	}
+	return alb, nil
 }
